4090fe5380b0ee601ce4be9bddd6eb5f1338ef22
Oliver Marketos <Oliver.marketos@gmail.com>
2021-12-13 13:33:44 +0200

    Set up GUI

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -1,3 +1,38 @@
-public class FractalExplorer {
-    
+/**
+ * A Fractal Explorer created in Java 
+ * Followed from youtube tutorial by William Fiset
+ * @author Oliver Marketos, olivermarketos@gmail.com
+ **/
+
+import java.util.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FractalExplorer extends JFrame {
+
+    static final int WIDTH= 1000;
+    static final int HEIGHT = 600;
+
+    public FractalExplorer(){
+        setInitialGUIProperties();
+    }
+
+    public void setInitialGUIProperties(){
+        this.setTitle("Fractal Explorer");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH, HEIGHT);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        
+    }
+
+
+
+
+    public static void main(String[] args) {
+        new FractalExplorer();
+    }
 }
